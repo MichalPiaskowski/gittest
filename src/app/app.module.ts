0dab1f17b5aa3507c444df4b30b7cf9e6c92619f
Michal Piaskowski <michal.piaskowski@softwareplant.com>
2019-06-03 14:44:44 +0200

    Initializing commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GitTestComponent } from './git-test/git-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitTestComponent,
+
   ],
   imports: [
     BrowserModule
